6bad5963906fa2b9d3278105dac24309272db85e
Leonel <leonelmmorgado@gmail.com>
2023-11-21 17:41:05 -0300

    added insertion sort implementation

---
+++ binary_search/target/debug/deps/binary_search-1a629d61fb6d6b65.d
@@ -0,0 +1,5 @@
+C:\Users\leone\Documents\Rust\Algos\binary_search\target\debug\deps\binary_search-1a629d61fb6d6b65.rmeta: src\main.rs
+
+C:\Users\leone\Documents\Rust\Algos\binary_search\target\debug\deps\binary_search-1a629d61fb6d6b65.d: src\main.rs
+
+src\main.rs:
---
+++ binary_search/target/debug/deps/binary_search-8900a293ce7b03b5.d
@@ -0,0 +1,5 @@
+C:\Users\leone\Documents\Rust\Algos\binary_search\target\debug\deps\binary_search-8900a293ce7b03b5.rmeta: src\main.rs
+
+C:\Users\leone\Documents\Rust\Algos\binary_search\target\debug\deps\binary_search-8900a293ce7b03b5.d: src\main.rs
+
+src\main.rs:
